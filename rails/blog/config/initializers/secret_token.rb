4ba299e629f59fb7161b449ad2a4caca9302c996
S. Hendriks <stefanhen83@gmail.com>
2012-12-22 22:08:36 +0100

    Let bowling game test pass. Oh and additionally add some rails blog for testing purposes.

---
+++ rails/blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '91481ed12a24969ca4cf5b6831ce76e6ff9195f95e674cc4f6fa1d609e78110bc4d678b52efa6b470521efd06693e12060bce2d430faea0c8f2b49171cc2754b'
