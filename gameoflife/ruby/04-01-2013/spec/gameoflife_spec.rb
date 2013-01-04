8ffb64748ecbf4256b785bf4b1e7e871f652ac84
Stefan Hendriks <stefanhen83@gmail.com>
2013-01-04 09:19:12 +0100

    all cases of dying by too many neighbours

--- gameoflife/ruby/04-01-2013/spec/gameoflife_spec.rb
+++ gameoflife/ruby/04-01-2013/spec/gameoflife_spec.rb
@@ -16,9 +16,9 @@ describe GameOfLife do
 			end
 
 			context "dies with more than three live neighbours" do
-				[4].each do |live_neighbours|
+				[4,5,6,7,8].each do |live_neighbours|
 					it "dies with #{live_neighbours}" do
-						dies(live_neighbours).should be_true	
+						dies(live_neighbours).should be_true, "expected to die with #{live_neighbours} live neighbours"	
 					end
 				end
 			end
@@ -31,7 +31,7 @@ describe GameOfLife do
 		end
 
 		def dies(live_neighbours)
-			live_neighbours < 2 || live_neighbours == 4
+			live_neighbours < 2 || live_neighbours >= 4
 		end
 	end
 
