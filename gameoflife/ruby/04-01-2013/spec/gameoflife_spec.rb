bd6a8a9fe239157973b7cfe01e0be537f5229289
Stefan Hendriks <stefanhen83@gmail.com>
2013-01-04 09:06:13 +0100

    add cell dies with fewer than 2 neighbours

--- gameoflife/ruby/04-01-2013/spec/gameoflife_spec.rb
+++ gameoflife/ruby/04-01-2013/spec/gameoflife_spec.rb
@@ -5,7 +5,12 @@ describe GameOfLife do
 	context "generation evaluation" do
 		
 		context "alive cell" do
-			it "dies with fewer than two live neighbours, as if caused by underpopulation."
+			it "dies with fewer than two live neighbours, as if caused by underpopulation." do
+				live_neighbours = 1
+				dies = live_neighbours < 2
+				dies.should be_true
+			end
+			
 			it "dies with more than three live neighbours"
 			it "lives with two or three live neighbours"
 		end
